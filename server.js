434effbe2d260033a5d913f3815c6b4e70a4061d
Luis Tormes <luisangeltormes@gmail.coom>
2021-09-10 11:10:25 -0400

    arreglando el cors

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static(path.join(__dirname) + "/public"));
 
 // cors
-app.use(cors());
+app.use('*',cors());
 
 // Logger
 app.use(logger("dev"));
